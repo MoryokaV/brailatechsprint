10f5aa06afba1c5f8f04ab2f5d723d1893f772a0
andreiusq <mrandyadv@gmail.com>
2023-02-24 11:58:32 +0200

    Basic logic added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cngmm.ro', 'imgur.com', 'i.imgur.com', 'archive.andreiusq.dev'],
+  }
 }
 
 module.exports = nextConfig
